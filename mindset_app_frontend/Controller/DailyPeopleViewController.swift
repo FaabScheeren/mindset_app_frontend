9216928c47c6975838a5ffd78cbef54aa35b7fb7
FaabScheeren <f.scheeren@outlook.com>
2020-07-21 12:35:11 +0200

    Create manager and view for people to connect

--- mindset_app_frontend/Controller/DailyPeopleViewController.swift
+++ mindset_app_frontend/Controller/DailyPeopleViewController.swift
@@ -7,24 +7,54 @@
 //
 
 import UIKit
+var Persons = [CheckQuestionData]()
+var personManager = PersonManager()
 
 class DailyPeopleViewController: UIViewController {
-
+    @IBOutlet weak var FirstPerson: UITextView!
+    @IBOutlet weak var SecondPerson: UITextView!
+    @IBOutlet weak var ThirdPerson: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        FirstPerson.delegate = self
+        SecondPerson.delegate = self
+        ThirdPerson.delegate = self
+
+        Persons.append(CheckQuestionData(title: "", done: false))
+        Persons.append(CheckQuestionData(title: "", done: false))
+        Persons.append(CheckQuestionData(title: "", done: false))
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func GoToDailyOverviewScreen(_ sender: Any) {
+        print("Tasks: \(Tasks)")
+            personManager.savePersons(with: Persons) { (succes) in
+                if (succes) {
+                    print("Finished!")
+                    DispatchQueue.main.async {
+                    self.performSegue(withIdentifier: "ToPersonScreen", sender: sender)
+                    }
+                } else {
+    //                errorLabel.text = "Something went wrong sorry."
+                    print("Error")
+                }
+            }
     }
-    */
+}
 
+extension DailyPeopleViewController: UITextViewDelegate {
+    func textViewDidChange(_ textView: UITextView) {
+        print(textView)
+        if textView == FirstPerson {
+            Persons[0] = (CheckQuestionData(title: FirstPerson.text, done: false))
+            print(Persons)
+        } else if textView == SecondPerson {
+            Persons[1] = (CheckQuestionData(title: SecondPerson.text, done: false))
+            print(Persons)
+        } else if textView == ThirdPerson {
+            Persons[2] = (CheckQuestionData(title: ThirdPerson.text, done: false))
+            print(Persons)
+        }
+    }
 }
